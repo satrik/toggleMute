a70c00c75809b3dd17059497717805f08060c617
satrik <dev@petrik.de>
2025-06-05 17:24:55 +0200

    new version

--- createDmg.sh
+++ createDmg.sh
@@ -7,4 +7,4 @@ cp ../README.md App.xcarchive/Products/Applications/README.md
 chmod +x App.xcarchive/Products/Applications/toggleMuteDisableQuarantine.command
 ln -s /Applications/ App.xcarchive/Products/Applications/Applications
 hdiutil create -volname "toggleMute" -srcfolder "App.xcarchive/Products/Applications/" -ov -format UDZO "../toggleMute.dmg"
-echo "DMG_CHECKSUM: $(shasum -a 256 -b "../toggleMute.dmg" | awk '{print $1 }')"
+echo -e "$(cat ../changes)\n\nDMG_CHECKSUM: $(shasum -a 256 -b "../toggleMute.dmg" | awk '{print $1 }')"
