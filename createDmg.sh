955f4c5cdb5a11afdacb86e5fa0ffe777c0c8289
satrik <dev@petrik.de>
2025-06-11 08:29:27 +0200

    feat: add img folder to dmg

--- createDmg.sh
+++ createDmg.sh
@@ -4,6 +4,7 @@ xcodebuild clean build analyze -scheme "toggleMute" -project "toggleMute.xcodepr
 xcodebuild archive -scheme "toggleMute" -archivePath App.xcarchive
 cp ../toggleMuteDisableQuarantine.command App.xcarchive/Products/Applications/toggleMuteDisableQuarantine.command
 cp ../README.md App.xcarchive/Products/Applications/README.md
+cp -r ../img App.xcarchive/Products/Applications/img
 chmod +x App.xcarchive/Products/Applications/toggleMuteDisableQuarantine.command
 ln -s /Applications/ App.xcarchive/Products/Applications/Applications
 hdiutil create -volname "toggleMute" -srcfolder "App.xcarchive/Products/Applications/" -ov -format UDZO "../toggleMute.dmg"
